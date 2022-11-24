25f1a7b809ce7b93128cea646259bdac48376ac5
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-11-25 02:02:03 +0530

    Connection with Mongo established, product data class created

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettingd:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettingd:DatabaseName"));
+            var collectonName = configuration.GetValue<string>("DatabaseSettingd:CollectionName");
+
+            Products = database.GetCollection<Product>(collectonName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
