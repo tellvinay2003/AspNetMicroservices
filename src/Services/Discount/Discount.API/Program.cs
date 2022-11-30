884c1c646602bcfd5be343cd0fe49ff376445e32
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-11-30 13:09:12 +0530

    DiscountAPI integrated with PostgreSql

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +14,14 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host =  CreateHostBuilder(args)
+                .Build();
+
+            // migrating our PostgreSql database, creating discountDb and Coupon table
+            host.MigrateDatabase<Program>();
+            host.Run();
+
+            // CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
