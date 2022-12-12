e18dd0454d26cbc8fb5ce67b8cd34bdb9fb11ca9
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-12-13 01:03:52 +0530

    Ordering microservice implemented folowing CQRS and CLEAN architecture

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hello world");
+
+
+
+public class MathFunctions
+{
+
+
+    public static void Main(string[] args)
+    {
+        string strName = "vinay";
+        string result = strName.ChangeFirstLetterCase();
+    }
+
+
+
+
+}
+
+
+public static class StringHelper
+{
+
+    public static string ChangeFirstLetterCase(this string inputString)
+    {
+        if (inputString.Length > 0)
+        {
+            char[] charArray = inputString.ToCharArray();
+            charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+            return new string(charArray);
+        }
+
+        return inputString;
+    }
+}
--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 namespace Ordering.API.Controllers
 {
     [ApiController]
-    [Route("api/v1/[controler]")]
+    [Route("api/v1/[controller]")]
     public class OrderController : ControllerBase
     {
         private readonly IMediator _mediator;
