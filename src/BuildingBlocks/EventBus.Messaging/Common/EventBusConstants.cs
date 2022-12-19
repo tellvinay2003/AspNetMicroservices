e6d020ed7e977162a416b9be7ba0096875222781
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-12-19 20:35:57 +0530

    created BuildingBlocks as CrossCutting concerns for messaging framework.

---
+++ src/BuildingBlocks/EventBus.Messaging/Common/EventBusConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messaging.Common
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketCheckout-queue";
+    }
+}
